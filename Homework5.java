35529bf466193b803879b7b2c97017fd86e7ade6
ReFeeDD <91897932+ReFeeDD@users.noreply.github.com>
2022-05-24 18:01:33 +0300

    Add files via upload

---
+++ Homework5.java
@@ -0,0 +1,24 @@
+
+import java.math.BigInteger;
+
+public class Homework5 {
+    String Name;
+    String Position;
+    String Email;
+    long Number;
+    int Salary;
+    int age;
+
+    public Homework5(String Name, String Position, String Email, int Number, int Salary, int age){
+        this.Name = Name;
+        this.Position = Position;
+        this.Email = Email;
+        this.Number = Number;
+        this.Salary = Salary;
+        this.age = age;
+        System.out.println("Name: " + Name + "\n Position: " + Position + "\n Email: " + Email + "\n Number: " + Number
+                + "\n Salary: " + Salary + "\n Age: " + age);
+    }
+
+
+}
